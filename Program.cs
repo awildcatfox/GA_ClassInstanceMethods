577d8d355a2775f68c942202dff3c944dc57cbb9
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2024-01-30 13:38:43 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GA_ClassInstanceMethods
+{/// David Abarca <summary>
+/// 1/30/2024
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
